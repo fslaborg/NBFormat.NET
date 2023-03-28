522bcd8d58f15747b4cea9289ed6156e4175c9ac
Kevin Schneider <schneider.kev@outlook.de>
2023-03-28 09:22:48 +0200

    wip html conversion

---
+++ src/FSharp.Data.NBConvert/HTML.fs
@@ -0,0 +1,9 @@
+﻿namespace FSharp.Data.NBConvert
+
+open FSharp.Data.NBFormat
+open FSharp.Data.NBFormat.Domain
+open Giraffe.ViewEngine
+
+module HTML =
+    let convertNotebook (notebook: Notebook) = 
+        HTMLTemplates.Default.renderNotebook notebook
